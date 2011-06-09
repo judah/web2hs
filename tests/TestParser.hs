bfc7dfab5546ecf12d6693f61971e4835ec4314b
Judah Jacobson <judah.jacobson@gmail.com>
2011-06-08 23:38:05 -0700

    Starting to write a pretty-printer for Pascal.
    Ignore-this: 66e816c12f8a72f1e316e882f7e6ed56
    
    darcs-hash:20110609063805-9f663-711d6407c544dfa63dee4fdd6659bfdeeb2daf08

--- tests/TestParser.hs
+++ tests/TestParser.hs
@@ -3,6 +3,9 @@ module TestParser where
 import Language.Pascal.Parser
 import Language.Pascal.Lexer
 import Language.Pascal.Syntax
+import Language.Pascal.Pretty
 
-testParser :: Show a => (Alex a) -> String -> IO ()
-testParser f = print . flip runAlex f
+testParser :: Pretty a => (Alex a) -> String -> IO ()
+testParser f prog = case runAlex prog f of
+                        Left err -> print err
+                        Right x -> print $ pretty x
