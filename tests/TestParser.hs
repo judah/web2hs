5a3842657be27f48cabbed4a69727d4bcdae3d41
Judah Jacobson <judah.jacobson@gmail.com>
2011-06-11 16:27:42 -0700

    Automate the parsing tests.
    Ignore-this: e7aa92c665be8ab77b544ad866f77573
    
    darcs-hash:20110611232742-9f663-b54c7c296d92f859b977e81ca59b53c1b7fd83e0

--- tests/TestParser.hs
+++ tests/TestParser.hs
@@ -4,8 +4,35 @@ import Language.Pascal.Parser
 import Language.Pascal.Lexer
 import Language.Pascal.Syntax
 import Language.Pascal.Pretty
+import Text.PrettyPrint.HughesPJ
+
+import Control.Monad
+import System.FilePath
 
 testParser :: Pretty a => (Alex a) -> String -> IO ()
 testParser f prog = case runAlex prog f of
                         Left err -> print err
                         Right x -> print $ pretty x
+
+testPrograms = [ "pooltype.p"
+               , "tangle.p"
+               , "weave.p"
+               , "dvitype.p"
+               ]
+
+runTests :: IO ()
+runTests = forM_ testPrograms $ \prog -> do
+    old <- readFile prog
+    putStrLn $ "==== " ++ prog ++ " ===="
+    case runAlex old program of
+        Left err -> print err
+        Right s -> do
+                    putStrLn "Succeeded."
+                    writeFile (replaceExtension prog "out")
+                        $ render $ pretty s
+                    {-
+                    writeFile (replaceExtension prog "diff.old")
+                        $ concat $ lines old
+                    writeFile (replaceExtension prog "diff.new")
+                        $ renderStyle style {mode=OneLineMode} $ pretty s
+                    -}
