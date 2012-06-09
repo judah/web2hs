c819ce6237279191afea48061c3a7a143e81c827
Judah Jacobson <judah.jacobson@gmail.com>
2012-06-09 16:41:58 -0700

    Starting on a test file for dvitype.
    Ignore-this: 5fc8ed3790f065255e429367a021d136
    
    darcs-hash:20120609234158-9f663-8b59a0313f12934d17e1e6a5b51e75fa7a0f63b5

---
+++ tests/dvitype.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+set -e
+
+CBITS=../cbits
+OUTPUTS=outputs
+
+runghc TestParser.hs pascal/dvitype.p $OUTPUTS/dvitype.c
+#gcc -c -I$CBITS $CBITS/builtins.c -o $OUTPUTS/builtins.o
+#gcc -c -I$CBITS $OUTPUTS/primes_web.c -o $OUTPUTS/primes_web.o
+#ghc -e main $OUTPUTS/builtins.o $OUTPUTS/primes_web.o Primes.hs \
+#    -odir $OUTPUTS -hidir $OUTPUTS
+
