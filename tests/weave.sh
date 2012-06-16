0cc3f0ecaea70848d5afda41c10c04cdc9f66836
Judah Jacobson <judah.jacobson@gmail.com>
2012-06-16 10:16:35 -0700

    Implement weave.
    Ignore-this: 7d614c86abc27f2cfe78da29df31637a
    
    darcs-hash:20120616171635-9f663-655ae273457611da97bd6a4bb7c7f2de58247f47

---
+++ tests/weave.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -x
+set -e
+
+CBITS=../cbits
+INPUTS=inputs
+OUTPUTS=outputs
+CC=clang
+GHC=ghc
+RUNGHC=runghc
+
+$RUNGHC TestParser.hs pascal/weave.p $OUTPUTS/weave_web.c
+$CC -c -I$CBITS $CBITS/builtins.c -o $OUTPUTS/builtins.o
+$CC -c -I$CBITS $OUTPUTS/weave_web.c -o $OUTPUTS/weave_web.o
+$GHC --make $OUTPUTS/builtins.o $OUTPUTS/weave_web.o Weave.hs \
+    -main-is Weave.main \
+    -o $OUTPUTS/Weave -odir $OUTPUTS -hidir $OUTPUTS
+$OUTPUTS/Weave $INPUTS/primes.web $INPUTS/primes.ch $OUTPUTS/primes.tex
+$OUTPUTS/Weave $INPUTS/tangle.web $INPUTS/tangle.ch $OUTPUTS/tangle.tex
+
