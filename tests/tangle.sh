0e15aad5a2d99ee8f1899e2d3ce6e2068c02a1a4
Judah Jacobson <judah.jacobson@gmail.com>
2012-06-17 14:25:27 -0700

    Use a .ch file for tangle.
    Ignore-this: f699f6dacd26a92f94911b95295d8294
    
    We now generate the same output as texlive's tangle.
    
    darcs-hash:20120617212527-9f663-45765198805c325f1cf2745ef91d702be9ce4080

--- tests/tangle.sh
+++ tests/tangle.sh
@@ -9,12 +9,15 @@ CC=clang
 GHC=ghc
 RUNGHC=runghc
 
-$RUNGHC TestParser.hs pascal/tangle.p $OUTPUTS/tangle_web.c
+tangle -underline web/tangle.web web/tangle.ch
+mv tangle.p $OUTPUTS/tangle.p
+$RUNGHC TestParser.hs $OUTPUTS/tangle.p $OUTPUTS/tangle_web.c
 $CC -c -I$CBITS $CBITS/builtins.c -o $OUTPUTS/builtins.o
 $CC -c -I$CBITS $OUTPUTS/tangle_web.c -o $OUTPUTS/tangle_web.o
 $GHC --make $OUTPUTS/builtins.o $OUTPUTS/tangle_web.o Tangle.hs \
     -main-is Tangle.main \
     -o $OUTPUTS/Tangle -odir $OUTPUTS -hidir $OUTPUTS
-$OUTPUTS/Tangle $INPUTS/primes.web $INPUTS/primes.ch $OUTPUTS/primes.p $OUTPUTS/primes.pool
-$OUTPUTS/Tangle $INPUTS/tangle.web $INPUTS/tangle.ch $OUTPUTS/tangle.p $OUTPUTS/tangle.pool
+$OUTPUTS/Tangle $INPUTS/primes.web $INPUTS/primes.ch $OUTPUTS/primes-test.p $OUTPUTS/primes-test.pool
+$OUTPUTS/Tangle web/tangle.web web/tangle.ch $OUTPUTS/tangle-test.p $OUTPUTS/tangle-test.pool
 
+diff $OUTPUTS/tangle-test.p $OUTPUTS/tangle.p && echo "Outputs are same."
