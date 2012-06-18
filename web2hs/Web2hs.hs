620ec4ac027ae704c7646d0e1dc940a98d6b5158
Judah Jacobson <judah.jacobson@gmail.com>
2012-06-17 17:24:43 -0700

    Use cmdargs, and add a --dump-pascal flag.
    Ignore-this: ad4a6de51a4947b13eec28fe52cccda8
    
    darcs-hash:20120618002443-9f663-2d7e2e7de6a684452c6035741c81beb64f31ced5

--- web2hs/Web2hs.hs
+++ web2hs/Web2hs.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Language.Pascal.Parser
+import qualified Language.Pascal.Parser as Parser
 import Language.Pascal.Lexer
 import Language.Pascal.Syntax
 import Language.Pascal.Transform
@@ -8,16 +8,42 @@ import Language.Pascal.Pretty
 import Language.Pascal.GenerateC
 import Text.PrettyPrint.HughesPJ
 
+import System.Console.CmdArgs
+
 import Control.Monad
 import System.FilePath
 import System.Environment
 
+data Options = Options {
+        inputFile :: String,
+        outputFile :: String,
+        pascalDumpFile :: Maybe String
+    } deriving (Show,Typeable,Data)
+
+getOptions = cmdArgs $
+                Options
+                    { inputFile = def &= argPos 0
+                                    &= typ "INPUT.p"
+                    , outputFile = def &= argPos 1
+                                    &= typ "OUTPUT.c"
+                    , pascalDumpFile = def
+                        &= explicit
+                        &= name "dump-pascal"
+                        &= typFile
+                        &= opt (Nothing::Maybe String)
+                    } &= program "web2hs"
+
+
 main = do
-    [progIn, progOut] <- getArgs
-    old <- readFile progIn
-    case runAlex old program of
+    Options {..} <- getOptions
+    old <- readFile inputFile
+    case runAlex old Parser.program of
         Left err -> print err
-        Right s -> writeFile progOut $ render
-                    $ generateProgram
-                    $ resolveProgram
-                    $ flattenProgram s
+        Right s -> do
+            let transformed = resolveProgram
+                                $ flattenProgram s
+            case pascalDumpFile of
+                Nothing -> return ()
+                Just f -> writeFile f $ render $ pretty transformed
+            writeFile outputFile
+                $ render $ generateProgram transformed
