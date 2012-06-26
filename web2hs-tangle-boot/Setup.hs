96967922e8fb5218897187aeb40a6bf1f7f58e4f
Judah Jacobson <judah.jacobson@gmail.com>
2012-06-25 23:55:47 -0700

    Fix and clean up the cabal-related code.
    We now generate all temp files under "dist" so they're automatically
    cleaned.
    cabal sdist should now work for all the repos.

--- web2hs-tangle-boot/Setup.hs
+++ web2hs-tangle-boot/Setup.hs
@@ -1,22 +1,29 @@
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Utils (createDirectoryIfMissingVerbose)
+import Distribution.Simple.Setup (fromFlag, buildVerbosity)
+import Distribution.PackageDescription
 import System.Process (rawSystem)
+import System.FilePath ( (</>) )
 import Control.Monad (when)
-import System.Directory (doesFileExist, removeFile)
 
 main = defaultMainWithHooks myHooks
 
 myHooks = simpleUserHooks
-            { buildHook = \pd lbi hooks' flags -> do
-                        generateTangle
-                        buildHook simpleUserHooks pd lbi hooks' flags
-            , cleanHook = \packageDescript () hooks flags -> do
-                            mapM_ removeFileIfExists ["tangle.c","tangle.h"]
-                            cleanHook simpleUserHooks packageDescript () hooks flags
+            { 
+            buildHook = \pd lbi hooks' flags -> do
+                            createDirectoryIfMissingVerbose (fromFlag (buildVerbosity flags))
+                                    True (parentDir lbi)
+                            generateTangle lbi
+                            buildHook simpleUserHooks (addCFile lbi pd) lbi hooks' flags
             }
 
-generateTangle = do
-    rawSystem "web2hs" ["tangle.p","tangle.c"]
+tanglePascalSrc = "tangle.p"
+parentDir = buildDir
+tangleCLoc lbi = buildDir lbi </> "tangle.c"
 
-removeFileIfExists f = do
-    exists <- doesFileExist f
-    when exists $ removeFile f
+generateTangle lbi = do
+    rawSystem "web2hs" [tanglePascalSrc, tangleCLoc lbi]
+
+addCFile lbi pd@PackageDescription {executables = [exec@Executable{buildInfo=bi}]}
+    = pd {executables = [exec {buildInfo = bi { cSources = [tangleCLoc lbi] ++ cSources bi}}]}
--- web2hs/Distribution/Web2hs.hs
+++ web2hs/Distribution/Web2hs.hs
@@ -1,16 +1,21 @@
 module Distribution.Web2hs(web2hsUserHooks) where
 
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Utils (createDirectoryIfMissingVerbose)
+import Distribution.Simple.Setup (fromFlag, buildVerbosity)
 import Distribution.PackageDescription
 import System.Process (readProcessWithExitCode, showCommandForUser)
 import System.Exit (ExitCode(..), exitWith)
-import System.FilePath (replaceExtension)
-import System.Directory (doesFileExist, removeFile)
+import System.FilePath (replaceExtension, takeBaseName, (<.>), (</>) )
 import System.IO (hPutStr, hPutStrLn, stderr, stdout)
-import Control.Monad (when)
+import Control.Monad (when, forM)
 
-web2hsWebFiles :: PackageDescription -> [String]
-web2hsWebFiles p = [f | ("x-web2hs-source",f) <- customFieldsPD p]
+web2hsWebFiles :: Executable -> [String]
+web2hsWebFiles e = [f | ("x-web2hs-source",f) <- customFieldsBI $ buildInfo e]
+
+web2hsBuildDir :: LocalBuildInfo -> Executable -> FilePath
+web2hsBuildDir lbi e = buildDir lbi </> exeName e </> "web2hs"
 
 -- For bootstrapping tangle, must call the helper "web2hs-tangle-boot"
 tangleProgram :: PackageDescription -> String
@@ -18,37 +23,42 @@ tangleProgram p = let
     tangles = [p | ("x-web2hs-tangle",p) <- customFieldsPD p]
     in if null tangles then "web2hs-tangle" else last tangles
                     
--- | Looks for fields of the form
+-- | This changes the given hooks to look for executables with fields of the form
 -- 
 -- > x-web2hs-source: dvitype.web
 --
 -- For each such field, it uses tangle and web2hs to generate
--- the corresponding .c/.h file during the "configure" step.
--- Futhermore, it removes all generated files during the "clean" step.
+-- the corresponding .c/.h files.  (They are stored in "dist/build", and
+-- "cabal clean" will delete them along with everything else in those directories.)
+-- It also changes the executables to build against the generated files.
 web2hsUserHooks :: UserHooks -> UserHooks
 web2hsUserHooks hooks = hooks
-    { buildHook = \pd lbi hooks' flags -> do
-        let progs = web2hsWebFiles pd
-        mapM_ (generateC (tangleProgram pd)) progs
-        buildHook hooks pd lbi hooks' flags
-    , cleanHook = \packageDescript () hooks' flags -> do
-                    mapM_ removeProgFiles
-                        $ web2hsWebFiles packageDescript
-                    cleanHook hooks packageDescript () hooks' flags
+    {
+    buildHook = \pd lbi hooks' flags -> do
+        exes <- forM (executables pd) $ \exe -> do
+                    let bi = buildInfo exe
+                    let verbosity = fromFlag (buildVerbosity flags)
+                    let parent = web2hsBuildDir lbi exe
+                    createDirectoryIfMissingVerbose verbosity True parent
+                    cFiles <- forM (web2hsWebFiles exe) $ \f -> do
+                                let cOutput = parent </> takeBaseName f <.> "c"
+                                generateC (tangleProgram pd) f cOutput
+                                return cOutput
+                    return exe { buildInfo = bi
+                                    { cSources = cFiles ++ cSources bi
+                                    , includeDirs = parent : includeDirs bi
+                                    } 
+                               }
+        buildHook hooks
+            pd { executables = exes } lbi hooks' flags
     }
 
-generateC tangle prog = do
-    rawSystem' tangle
-        $ [prog] ++ map (replaceExtension prog) ["ch","p","pool"]
-    rawSystem' "web2hs"
-        $ map (replaceExtension prog) ["p","c"]
-
-removeProgFiles prog = mapM_ (removeFileIfExists . replaceExtension prog)
-                        ["p","pool","h","c"]
-
-removeFileIfExists f = do
-    exists <- doesFileExist f
-    when exists $ removeFile f
+generateC tangle webFile cFile = do
+    let changeFile = replaceExtension webFile "ch"
+    let pascalFile = replaceExtension cFile "p"
+    let poolFile = replaceExtension cFile "pool"
+    rawSystem' tangle [webFile, changeFile, pascalFile, poolFile]
+    rawSystem' "web2hs" [pascalFile, cFile]
 
 rawSystem' p args = do
     hPutStrLn stdout $ showCommandForUser p args
